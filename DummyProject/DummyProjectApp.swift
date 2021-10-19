40b92834dfd751aeb314581654345e213391e08b
Shuvayan Saha <shuvayansaha@Shuvayans-MacBook-Air.local>
2021-10-19 17:38:01 +0530

    feat: project setup

---
+++ DummyProject/DummyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyProjectApp.swift
+//  DummyProject
+//
+//  Created by Shuvayan Saha on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DummyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
